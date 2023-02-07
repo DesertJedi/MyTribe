35185fc78e821fd1a4c2ec3ec82780575b4d581d
DesertJedi <brandnewruss@gmail.com>
2023-02-07 13:31:44 -0700

    Top menu added

--- index.php
+++ index.php
@@ -2,22 +2,27 @@
 <html>
 <meta charset='UTF-8'>
 <title>NomadConnect</title>
-<link rel="stylesheet" href="assets\css\menu.css">
-<script src="assets\js\main.js"></script>
+<head>
+    <link rel='stylesheet' href='assets/css/menu.css'>
+    <link href="vendor/fontawesome/css/fontawesome.css" rel="stylesheet">
+    <link href="vendor/fontawesome/css/regular.css" rel="stylesheet">
+    <link href="vendor/fontawesome/css/solid.css" rel="stylesheet">
 
+    <script src='assets/js/main.js'></script>
+</head>
 <body>
     <nav id='menu'>
         <input type='checkbox' id='responsive-menu' onclick='updatemenu()'><label></label>
         <ul>
             <li><a href='http://'>Home</a></li>
-            <li><a href='http://'>Profile</a></li>
+            <!-- TODO: When between phone width and half width not all icons are showing - make a few different view sizes so everything can be seen at any width
             <!-- TODO: Scrolling from expanded Events to Groups goes past Groups so it doesn't expand - maybe fix -->
             <li><a class='dropdown-arrow' href='http://'>Events</a>
-            <ul class='sub-menus'>
-                <li><a href='http://'>My Events</a></li>
-                <li><a href='http://'>New Events</a></li>
-                <li><a href='http://'>Search Events</a></li>
-            </ul>
+                <ul class='sub-menus'>
+                    <li><a href='http://'>My Events</a></li>
+                    <li><a href='http://'>New Events</a></li>
+                    <li><a href='http://'>Search Events</a></li>
+                </ul>
             </li>
             <li><a class='dropdown-arrow' href='http://'>Groups</a>
                 <ul class='sub-menus'>
@@ -25,7 +30,9 @@
                     <li><a href='http://'>Search Groups</a></li>
                 </ul>
             </li>
-            <li><a href='http://'>Messages</a></li>
+            <li><a href='http://'><i class="fa-regular fa-message"></i></a></li>
+            <li><a href='http://'><i class='fa-regular fa-user'></i></a></li>
+            <li><a href='http://'><i class='fa-regular fa-hand-peace'></i></a></li>
         </ul>
     </nav>
 </body>
