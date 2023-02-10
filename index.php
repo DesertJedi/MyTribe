469d14b383bf603720ed0b739b06610bf2d6921c
DesertJedi <brandnewruss@gmail.com>
2023-02-10 16:48:10 -0700

    tribe update cards added, and ready to be tweaked.

--- index.php
+++ index.php
@@ -1,40 +1,124 @@
 <!DOCTYPE html>
 <html>
 <meta charset='UTF-8'>
-<title>NomadConnect</title>
+<title>My Tribe</title>
 <head>
+    <link rel='stylesheet' href='assets/css/main.css'>
     <link rel='stylesheet' href='assets/css/menu.css'>
-    <link href="vendor/fontawesome/css/fontawesome.css" rel="stylesheet">
-    <link href="vendor/fontawesome/css/regular.css" rel="stylesheet">
-    <link href="vendor/fontawesome/css/solid.css" rel="stylesheet">
+    <link rel='stylesheet' href='assets/css/cards.css'>
+    <link href='vendor/fontawesome/css/fontawesome.css' rel='stylesheet'>
+    <link href='vendor/fontawesome/css/regular.css' rel='stylesheet'>
+    <link href='vendor/fontawesome/css/solid.css' rel='stylesheet'>
+    <link href='vendor/fontawesome/css/brands.css' rel='stylesheet'>
+    <link href='vendor/bootstrap/bootstrap.min.css' rel='stylesheet'>
 
     <script src='assets/js/main.js'></script>
 </head>
 <body>
-    <nav id='menu'>
-        <input type='checkbox' id='responsive-menu' onclick='updatemenu()'><label></label>
-        <ul>
-            <li><a href='http://'>Home</a></li>
-            <!-- TODO: When between phone width and half width not all icons are showing - make a few different view sizes so everything can be seen at any width
-            <!-- TODO: Scrolling from expanded Events to Groups goes past Groups so it doesn't expand - maybe fix -->
-            <li><a class='dropdown-arrow' href='http://'>Events</a>
-                <ul class='sub-menus'>
-                    <li><a href='http://'>My Events</a></li>
-                    <li><a href='http://'>New Events</a></li>
-                    <li><a href='http://'>Search Events</a></li>
-                </ul>
-            </li>
-            <li><a class='dropdown-arrow' href='http://'>Groups</a>
-                <ul class='sub-menus'>
-                    <li><a href='http://'>My Groups</a></li>
-                    <li><a href='http://'>Search Groups</a></li>
-                </ul>
-            </li>
-            <li><a href='http://'><i class="fa-regular fa-message"></i></a></li>
-            <li><a href='http://'><i class='fa-regular fa-user'></i></a></li>
-            <li><a href='http://'><i class='fa-regular fa-hand-peace'></i></a></li>
-        </ul>
-    </nav>
+
+    <!-- begin logo section -->
+    <!-- end logo section -->
+
+    <!-- begin top nav menu -->
+<!--    <div id='page-header'>-->
+<!--        <div id='site-logo'>-->
+<!--            <img src='assets/images/logo_test.png' height='60px' width='120px'>-->
+<!--            <span id='logo'><i class='fa-brands fa-pagelines'> My Tribe</i> </span>-->
+<!--        </div>-->
+
+<!--    </div>-->
+    <!-- end top nav menu -->
+
+    <!-- begin recent tribe updates section -->
+    <ul class="cards">
+        <li>
+            <a href="" class="card">
+                <img src="assets/images/people_test_images/b1.jpg" class="" alt="" />
+                <div class="card__overlay">
+                    <div class="card__header">
+                        <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                        <img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" /> <!-- user's profile thumb -->
+                        <div class="card__header-text">
+                            <h3 class="card__title">Jessica Parker</h3> <!-- user's name -->
+                            <span class="card__status">1 hour ago</span> <!-- time since posted -->
+                        </div>
+                    </div>
+                    <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                </div>
+            </a>
+        </li>
+        <li>
+            <a href="" class="card">
+                <img src="assets/images/people_test_images/b2.jpg" class="card__image" alt="" />
+                <div class="card__overlay">
+                    <div class="card__header">
+                        <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                        <img class="card__thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
+                        <div class="card__header-text">
+                            <h3 class="card__title">kim Cattrall</h3>
+                            <span class="card__status">3 hours ago</span>
+                        </div>
+                    </div>
+                    <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                </div>
+            </a>
+        </li>
+        <li>
+            <a href="" class="card">
+                <img src="assets/images/people_test_images/b3.jpg" class="card__image" alt="" />
+                <div class="card__overlay">
+                    <div class="card__header">
+                        <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                        <img class="card__thumb" src="https://i.imgur.com/7D7I6dI.png" alt="" />
+                        <div class="card__header-text">
+                            <h3 class="card__title">Jessica Parker</h3>
+                            <span class="card__tagline">Lorem ipsum dolor sit amet consectetur</span> <!-- a short description that is shown over image - may be too big if image too small -->
+                            <span class="card__status">1 hour ago</span>
+                        </div>
+                    </div>
+                    <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                </div>
+            </a>
+        </li>
+        <li>
+            <a href="" class="card">
+                <img src="assets/images/people_test_images/b4.jpg" class="card__image" alt="" />
+                <div class="card__overlay">
+                    <div class="card__header">
+                        <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg>
+                        <img class="card__thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />
+                        <div class="card__header-text">
+                            <h3 class="card__title">kim Cattrall</h3>
+                            <span class="card__status">3 hours ago</span>
+                        </div>
+                    </div>
+                    <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, blanditiis?</p>
+                </div>
+            </a>
+        </li>
+    </ul>
+    <!-- end recent tribe updates section -->
+    <br />
+    <!-- begin right section -->
+    <div id='page_right_wrapper'>
+        <div class='anti-ad'>
+            <div class='anti-ad-header'>
+                Affirmation for Happiness
+            </div>
+            <div class='anti-ad-text'>
+                I am allowing myself to feel good.
+            </div>
+        </div>
+    </div>
+    <!-- end right section -->
+
+    <!-- begin footer -->
+<!--    <div>-->
+<!---->
+<!--    </div>-->
+    <!-- end footer -->
+
+
 </body>
 </html>
 
